affff0027abffd7be731bd3b6ac477ea458f9946
Takuya Kuhara <takuya.kuhataku@gmail.com>
2017-02-20 05:41:58 +0900

    Add branch-equal instruction datapath.

--- design/top/top.sv
+++ design/top/top.sv
@@ -4,6 +4,8 @@ module top (
 );
 
    reg [ 31 : 0 ] PC;
+   wire [ 31 : 0 ] PCPLUS4;
+   wire [ 31 : 0 ] PCBranch;
    wire [ 31 : 0 ] instr;
    wire [ 31 : 0 ] sign_imm;
    wire regwrite;
@@ -22,15 +24,21 @@ module top (
    wire alu_src;
    wire mem2reg;
    wire regdst;
+   wire branch;
+   wire PCSrc;
 
    assign rf_a1 = instr[25:21];
    assign rf_a2 = instr[20:16];
    assign rf_a3 = regdst ? instr[15:11] : instr[20:16];
    assign srcb =  alu_src ? sign_imm : reg_rd2;
    assign result = mem2reg ?  readdata : alu_result;
+   assign PCPLUS4 = PC + 4;
+   assign PCBranch = ( sign_imm << 2 ) + PCPLUS4;
+   assign PCSrc = zero * branch;
+   assign NextPC = PCSrc ? PCBranch : PCPLUS4;
 
 
-   rom imem(.A(PC), .RD(instr));
+   rom imem(.A(PC[31:2]), .RD(instr));
    regfile32x32 rf(.CLK(CLK), .A1(instr[25:21]), .A2(instr[20:16]), .A3(instr[20:16]), .WD3(readdata), .WE3(regwrite), .RD1(reg_rd1), .RD2(reg_rd2));
    ram dmem(.CLK(CLK), .A(alu_result), .WD(reg_rd2), .WE(memwrite), .RD(readdata));
    sign_extend se(.extend_in(instr[15:0]), .extend_out(sign_imm));
@@ -40,7 +48,7 @@ module top (
       if(RST) begin
          PC <= 0;
       end else begin
-         PC <= PC + 4;
+         PC <= NextPC;
       end
    end
 
