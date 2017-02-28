ccabc77d756ddea893792cf5b74568142c3dfa25
Takuya Kuhara <takuya.kuhataku@gmail.com>
2017-02-28 09:32:07 +0900

    Update alu and controller.

--- design/alu/alu.sv
+++ design/alu/alu.sv
@@ -13,10 +13,14 @@ module alu (
       input [ 31 : 0 ] srcb;
       input [  2 : 0 ] alu_control;
       begin
-         if ( alu_control == 3'b010 )
-            alu_body = srca + srcb;
-         else
-            alu_body = 0; //T.B.D.
+         case(alu_control)
+            3'b010: alu_body = srca + srcb;
+            3'b110: alu_body = srca - srcb;
+            3'b000: alu_body = srca & srcb;
+            3'b001: alu_body = srca | srcb;
+            3'b111: alu_body = srca < srcb;
+            default: alu_body = 32'bx;
+         endcase //(alu_control)
       end
    endfunction //alu_body
    
--- design/controller/controller.sv
+++ design/controller/controller.sv
@@ -20,11 +20,12 @@ module controller(
   function [ 7 : 0 ] main_decoder;
     input [ 5 : 0 ] opcode;
     begin
-      case (opcode)
+      casex (opcode)
         6'b000000: main_decoder = 8'b11000010;
         6'b100011: main_decoder = 8'b10100100;
         6'b101011: main_decoder = 8'b0x101x00;
         6'b000100: main_decoder = 8'b0x010x01;
+        6'b001000: main_decoder = 8'b10100000;
         default : main_decoder = 8'bxxxxxxxx;
       endcase
     end
@@ -34,14 +35,14 @@ module controller(
     input [ 1 : 0 ] aluop;
     input [ 5 : 0 ] funct;
     begin
-      case ({aluop,funct})
+      casex ({aluop,funct})
         {2'b00, 6'bxxxxxx}: alu_decoder = 3'b010;
         {2'b01, 6'bxxxxxx}: alu_decoder = 3'b110;
-        {2'b1x, 6'b100000}: alu_decoder = 3'b110;
+        {2'b1x, 6'b100000}: alu_decoder = 3'b010;
         {2'b1x, 6'b100010}: alu_decoder = 3'b110;
-        {2'b1x, 6'b100100}: alu_decoder = 3'b110;
-        {2'b1x, 6'b100101}: alu_decoder = 3'b110;
-        {2'b1x, 6'b101010}: alu_decoder = 3'b110;
+        {2'b1x, 6'b100100}: alu_decoder = 3'b000;
+        {2'b1x, 6'b100101}: alu_decoder = 3'b001;
+        {2'b1x, 6'b101010}: alu_decoder = 3'b111;
         default : alu_decoder = 3'bxxx;
       endcase
     end
